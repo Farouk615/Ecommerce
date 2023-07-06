6298a712881d09a5f9fdbd55541ebdabd996468d
lenovo <faroukayadi1@gmail.com>
2023-07-06 14:35:13 +0100

    finishing actions on category and completing product actions

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +22,7 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/new', name: 'app_category_new', methods: ['POST'])]
-    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    public function new(Request $request, CategoryRepository $categoryRepository, EntityManagerInterface $em): JsonResponse
     {
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
@@ -29,41 +30,36 @@ class CategoryController extends AbstractController
         $form->submit($data['category']);
         if ($form->isSubmitted() && $form->isValid()) {
             $categoryRepository->save($category, true);
-            return $this->json($category);
+            $em->flush();
+            return new JsonResponse(['status' => 'Category created'], Response::HTTP_CREATED);
         }
-        return $this->json("Category not created",400);
-    }
+        return new JsonResponse(['status' => 'Category not created'], Response::HTTP_CREATED);    }
 
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
     public function show(Category $category): Response
     {
-        return $this->render('category/show.html.twig', [
-            'category' => $category,
-        ]);
+        return $this->json($category);
     }
 
     #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository,
+                         EntityManagerInterface $em): JsonResponse
     {
         $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data['category']);
         if ($form->isSubmitted() && $form->isValid()) {
             $categoryRepository->save($category, true);
-
-            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+            $em->flush();
+            return new JsonResponse(['status' => 'Category updated'], Response::HTTP_OK);
         }
-
-        return $this->renderForm('category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
+        return new JsonResponse(['status' => 'Category updated'], Response::HTTP_OK);
     }
 
     #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
     public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
             $categoryRepository->remove($category, true);
         }
 
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/product')]
+class ProductController extends AbstractController
+{
+    #[Route('/', name: 'app_product_index', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): JsonResponse
+    {
+        return $this->json($productRepository->findAll());
+    }
+
+    #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProductRepository $productRepository,EntityManagerInterface $em): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data['product']);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->save($product, true);
+            $em->flush();
+            return new JsonResponse(['status' => 'Product created'], Response::HTTP_CREATED);
+        }
+        return new JsonResponse(['status' => 'Product not created'], Response::HTTP_BAD_REQUEST);
+    }
+
+    #[Route('/{id}', name: 'app_product_show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productRepository->save($product, true);
+
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_product_delete', methods: ['POST'])]
+    public function delete(Request $request, Product $product, ProductRepository $productRepository,EntityManagerInterface $em): Response
+    {
+        try{
+            $productRepository->remove($product);
+            $em->flush();
+            return new JsonResponse(['status' => 'Book removed'], Response::HTTP_OK);
+        }
+        catch(\TypeError $e){
+            return new JsonResponse(['status' => 'Book not removed Exception occured'], Response::HTTP_NOT_FOUND);
+        }
+    }
+}
